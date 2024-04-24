cbf2010aeba258d887dd7c40b6fe1f72af3e95e1
ryu-taesung <108068287+ryu-taesung@users.noreply.github.com>
2024-04-24 14:10:15 -0400

    Updates for MacOS

--- grab-txt.py
+++ grab-txt.py
@@ -18,14 +18,20 @@ update_splash('importing subprocess . . .')
 from subprocess import run as _run, PIPE as _PIPE #STARTUPINFO as _STARTUPINFO, STARTF_USESHOWWINDOW as _STARTF_USESHOWWINDOW
 update_splash('importing io . . .')
 from io import BytesIO as _BytesIO
-#update_splash('importing pystray . . .')
-#import pystray
-#from pystray import MenuItem as item
+update_splash('importing pystray . . .')
+pystray_loaded = False
+try:
+    import pystray
+    from pystray import MenuItem as _item
+    pystray_loaded = True
+except Exception as e:
+    print(f"Exception trying to load pystray: {e}")
 update_splash('importing os . . .')
 from os import path as _path, name as _name
 update_splash('import threading . . .')
 from threading import Thread as _Thread
-from time import sleep as _sleep
+update_splash('import sys.platform')
+from sys import platform as _platform
 
 class ScreenGrabWindow(tk.Toplevel):
     def __init__(self, grab_button):
@@ -39,10 +45,22 @@ class ScreenGrabWindow(tk.Toplevel):
         self.canvas = tk.Canvas(self, cursor="crosshair")
         self.canvas.pack(fill="both",expand=True)
 
-        #screencapture_command = ['screencapture', '-x', '-R', bbox_str, '/tmp/screenshot.png']
-        screencapture_command = ['grim', '/tmp/screenshot.png']
+        shrink = False
+        if _platform == 'linux':
+            screencapture_command = ['grim', '/tmp/screenshot.png']
+            #screencapture_command = ['screencapture', '-x', '-R', bbox_str, '/tmp/screenshot.png']
+        elif _platform == 'darwin':
+            screencapture_command = ['screencapture', '/tmp/screenshot.png']
+            shrink = True
         _run(screencapture_command, shell=False)
         self.screenshot_image = Image.open('/tmp/screenshot.png')
+        size = self.screenshot_image.size
+        if shrink:
+            size = size[0]//2, size[1]//2 #// is for integer division
+            print(f"Shrinking image. . . ")
+            print(f"Original size: {self.screenshot_image.size}")
+            print(f"New size: {size}.")
+            self.screenshot_image = self.screenshot_image.resize(size)
         self.image = ImageTk.PhotoImage(self.screenshot_image)
         self.photo = self.canvas.create_image(0,0,image=self.image,anchor="nw")
         self.x, self.y = 0, 0
@@ -73,14 +91,8 @@ class ScreenGrabWindow(tk.Toplevel):
         self.t1.start()
         
     def image_processing_thread(self, bbox):
-#        _sleep(1)
-#        # bbox format should be "x,y widthxheight"        
-#        bbox_str = f"{bbox[0]},{bbox[1]} {bbox[2]-bbox[0]}x{bbox[3]-bbox[1]}"
-#        scrot_command = ['grim','-g',bbox_str,'/tmp/screenshot_rect.png']
-#        _run(scrot_command, shell=False)
-# Load the image from the file saved by scrot
-#        screen_grab = Image.open('/tmp/screenshot_rect.png')
-        cropped_image = self.screenshot_image.crop(bbox)        
+        cropped_image = self.screenshot_image.crop(bbox)
+        #cropped_image.save('/tmp/screenshot_cropped.png')
 
         # Image processing
         larger = cropped_image.resize((cropped_image.width * 3, cropped_image.height * 3))
@@ -89,6 +101,8 @@ class ScreenGrabWindow(tk.Toplevel):
         contrast = ImageEnhance.Contrast(larger)
         contrast.enhance(2).save(buf, format='PNG', dpi=(300, 300))
         image_bytes = buf.getvalue()
+        # with open('/tmp/screenshot_larger.png', 'wb') as f:
+        #     f.write(image_bytes)
 
         # Update the GUI with the new image
         self.new_image = ImageTk.PhotoImage(cropped_image)
@@ -101,10 +115,13 @@ class ScreenGrabWindow(tk.Toplevel):
 
         if getattr(sys, 'frozen', False):
             application_path = sys._MEIPASS
-            tess = _path.join(application_path, "Tesseract-OCR", "tesseract.exe --psm 4 stdin stdout")
+            tesseract = _path.join(application_path, "Tesseract-OCR", "tesseract.exe --psm 4 stdin stdout")
         else:
-            tess = ("/usr/bin/tesseract","--psm","4","stdin","stdout")
-        print(f'tess: {tess}')
+            if _platform in ['linux', 'linux2']:
+                tesseract = ("/usr/bin/tesseract","--psm","4","stdin","stdout")
+            elif _platform == 'darwin':
+                tesseract = ('/opt/homebrew/bin/tesseract','--psm','4','stdin','stdout')
+        print(f'tesseract: {tesseract}')
         #tess = r"c:\Program Files\Tesseract-OCR\tesseract.exe --psm 4 stdin stdout"
         self.update()
         
@@ -115,7 +132,7 @@ class ScreenGrabWindow(tk.Toplevel):
             startupinfo.dwFlags |= _STARTF_USESHOWWINDOW
 
         rc = _run(
-            tess,
+            tesseract,
             input=image_bytes,
             stdout=_PIPE,
             stderr=None,
@@ -145,6 +162,7 @@ class Application(tk.Tk):
     def __init__(self, title):
         super().__init__()
         self.wm_attributes('-alpha', 0)
+        self.application_path = _path.dirname(__file__)
         if getattr(sys, 'frozen', False): #https://stackoverflow.com/a/45631436
             self.application_path = sys._MEIPASS
             try:
@@ -152,13 +170,17 @@ class Application(tk.Tk):
                     self.version_number = file.read()
             except:
                 self.version_number = "ERR"
-        elif __file__:
-            self.application_path = _path.dirname(__file__)
+        elif "__compiled__" in globals():
             try:
-                import get_git_version_tag
-            
-                self.version_number = get_git_version_tag.get_git_version_tag()
+                with open(_path.join(self.application_path,'version.txt'), 'r') as file:
+                    self.version_number = file.read()
             except:
+                self.version_number = "ERR"
+        elif __file__:
+            try:
+                import get_git_version_tag            
+                self.version_number = get_git_version_tag.get_git_version_tag()
+            except Exception as e:
                 self.version_number = "vEXP"
 
         self.grab_window = None
@@ -173,8 +195,8 @@ class Application(tk.Tk):
         #self.geometry('200x90')
         self.grab_button = tk.Button(self, text="Screenshot to Grab Text", command=self.create_screen_grabber, padx="10", pady="10")
         self.grab_button.pack(fill="both", expand=True, padx="20", pady="20")
-        #if sys.platform == 'win32':
-        #    self.protocol('WM_DELETE_WINDOW', self.hide_window) #on close window
+        if pystray_loaded:
+            self.protocol('WM_DELETE_WINDOW', self.hide_window) #on close window
         update_splash('Done!')
         self.wm_attributes('-alpha', 1)
 
@@ -183,22 +205,22 @@ class Application(tk.Tk):
             self.grab_window.close_window()
         self.grab_window = ScreenGrabWindow(self.grab_button)
 
-    #def quit_window(self, icon, item):
-    #    self.icon.stop()
-    #    self.destroy()
+    def quit_window(self, icon, item):
+        self.icon.stop()
+        self.destroy()
 
-    #def show_window(self, icon, item):
-    #    print('show')
-    #    self.icon.stop()
-    #    self.after(0,self.deiconify)
+    def show_window(self, icon, item):
+        #print('show')
+        self.icon.stop()
+        self.after(0,self.deiconify)
 
-    #def hide_window(self, *args, **kwargs):
-    #    self.withdraw()
+    def hide_window(self, *args, **kwargs):
+        self.withdraw()
         #print('minimized')
-    #    image = Image.open(_path.join(self.application_path, "icon.png"))
-    #    menu=(item('Show Window', self.show_window), item('Exit', self.quit_window), )
-    #    self.icon = pystray.Icon('text_grabber', image, 'Text Grabber', menu=menu)
-    #    self.icon.run()
+        image = Image.open(_path.join(self.application_path, 'icons', 'smiley-glass.png'))
+        menu=(_item('Show Window', self.show_window), _item('Exit', self.quit_window), )
+        self.icon = pystray.Icon('text_grabber', image, 'Text Grabber', menu=menu)
+        self.icon.run()
 
 root = Application("Text Grabber")
 
